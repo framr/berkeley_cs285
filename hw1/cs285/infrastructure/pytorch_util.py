1d9ea1c0d559a35c8fa2f4c2009731648a96b8ed
Pavel Shtykovskiy <pavel.sht@yandex.ru>
2022-05-03 05:53:18 +0200

    MLP Policy

--- hw1/cs285/infrastructure/pytorch_util.py
+++ hw1/cs285/infrastructure/pytorch_util.py
@@ -17,6 +17,22 @@ _str_to_activation = {
 }
 
 
+class MLPNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__(self, input_size, output_size, n_layers, size, activation_fn, output_activation_fn)
+        self.layers = [nn.Linear(input_size, size), activation_fn]
+        for _ in range(n_layers - 1):
+            self.layers.append(nn.Linear(size, size))
+            self.layers.append(activation_fn)
+        self.layers.append(nn.Linear(size, output_size))
+        self.layers.append(output_activation)
+
+    def forward(x):
+        for l in self.layers:
+            x = l(x)
+        return x
+
+
 def build_mlp(
         input_size: int,
         output_size: int,
@@ -45,10 +61,8 @@ def build_mlp(
     if isinstance(output_activation, str):
         output_activation = _str_to_activation[output_activation]
 
-    # TODO: return a MLP. This should be an instance of nn.Module
-    # Note: nn.Sequential is an instance of nn.Module.
-    raise NotImplementedError
 
+    return MLPNet(input_size, output_size, n_layers, size, activation, output_activation)
 
 device = None
 
